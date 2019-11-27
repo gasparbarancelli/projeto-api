989b751b9aea28ff1bdc625268ecb34656d3a985
felipeholek <damasceno28>
2019-11-27 20:39:08 -0300

    Adicionado o método que retorna o autorizador com menor disponibilidade

--- src/main/java/br/com/viasoft/projetoapi/repository/HistoricoRepository.java
+++ src/main/java/br/com/viasoft/projetoapi/repository/HistoricoRepository.java
@@ -1,7 +1,6 @@
 package br.com.viasoft.projetoapi.repository;
 
 import br.com.viasoft.projetoapi.model.Historico;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -11,4 +10,30 @@ import java.util.List;
 public interface HistoricoRepository extends JpaRepository<Historico, Long> {
     List<Historico> findByAutorizadorEquals(String autorizador);
     List<Historico> findByHoraBetween(LocalDateTime inicio, LocalDateTime fim);
+    /*
+    Consulta abaixo formatada:
+    ===============
+    select
+        y.autorizador
+    from
+        (select
+            max(x.offline) as conta,
+            x.autorizador
+        from
+            (select
+                autorizador,
+                sum(case when autorizacao = 1 then 1 end) as online,
+                sum(case when autorizacao in (0, 2) then 1 end) as offline
+            from
+                historico
+            group by
+                historico.autorizador) as x
+        group by x.autorizador) as y
+    where
+        conta is not null
+    ===============
+     */
+    @Query(value = "select y.autorizador from (select max(x.offline) as conta, x.autorizador from (select autorizador, sum(case when autorizacao = 1 then 1 end) as online, sum(case when autorizacao in (0, 2) then 1 end) as offline from historico group by historico.autorizador) as x group by x.autorizador) as y where conta is not null",
+            nativeQuery = true)
+    String findMostOffline();
 }
