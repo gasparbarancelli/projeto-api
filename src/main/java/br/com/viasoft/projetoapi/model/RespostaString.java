d7b7172a327b04cb25894a2e19703dd3f0cb6852
felipeholek <damasceno28>
2019-11-27 20:40:07 -0300

    Adicionada uma classe que possibilita o retorno da menor disp.

---
+++ src/main/java/br/com/viasoft/projetoapi/model/RespostaString.java
@@ -0,0 +1,13 @@
+package br.com.viasoft.projetoapi.model;
+
+public class RespostaString {
+    private String response;
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+}
